ceffe327de8a7b5afbeb51fade6547c20f85cbb8
Irina <politunira@mail.ru>
2026-04-05 19:42:25 +1100

    refactor(booking): move to OOP, isolate schema, remove forecast responsibility, add RabbitMQ events

--- src/booking/main.py
+++ src/booking/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
 from booking.api.routers.booking_router import router as booking_router
-from booking.api.routers.forecast_router import router as prediction_router
 from booking.api.routers.hotels import router as hotels_router
 from shared.errors import register_error_handlers, setup_openapi_with_errors
 
@@ -11,7 +10,6 @@ register_error_handlers(app)
 setup_openapi_with_errors(app)
 
 app.include_router(booking_router, prefix="/booking")
-app.include_router(prediction_router, prefix="/forecast")
 app.include_router(hotels_router, prefix="/hotel")
 
 
